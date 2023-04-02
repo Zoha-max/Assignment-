45b3271b97cd50e65f865069c3da0ca274b6f371
Zoha-max <127471377+Zoha-max@users.noreply.github.com>
2023-04-02 13:09:57 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,18 @@
+package com.cognizant.shapes;
+
+public class Rectangle {
+	
+	public int CalculateArea(int l,int w)
+	{
+		return l*w;
+		
+	}
+		
+
+public static void main(String[] args) {
+	System.out.println("The area of Reactangle is calculate by using formula length*breath");
+
+	Rectangle obj=new Rectangle();
+	//System.out.println(obj.CalculateArea(5,6));
+} 
+}
